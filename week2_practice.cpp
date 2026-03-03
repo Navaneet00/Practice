a041213590084862ef3e8f6316893bce4002ec1d
Navaneet <navaneetbammanni@gmail.com>
2026-03-03 22:46:24 +0530

    Update week2_practice.cpp

--- week2_practice.cpp
+++ week2_practice.cpp
@@ -1,6 +1,9 @@
 #include <thread>
 #include <iostream>
 #include <mutex>
+#include <chrono>
+#include <future>
+#include <queue>
 #include <condition_variable>
 using namespace std;
 
@@ -75,6 +78,138 @@ void getId(int num){
     cout << "Thread " << num << " is running!" << endl;
 }
 
+// Use of future class template and async function template
+int get_the_answer(){
+    this_thread::sleep_for(chrono::seconds(2));
+    return 45;
+}
+
+// Using future with promise
+void get_answer(promise<int> p){
+    p.set_value(44);
+}
+
+// Thread Pool
+class ThreadPool{
+    public:
+
+    // Constructor to create a thread pool with given number of threads
+    ThreadPool(size_t num_threads = thread::hardware_concurrency())
+    {
+        // Creating worker threads
+        for (size_t i = 0; i < num_threads; i++){
+            threads.emplace_back([this] {
+                while(true){
+                    function<void()> task;
+
+                    {
+                        // Locking the queue so that data can be shared safely
+                        unique_lock<mutex> lock(queue_mutex);
+
+                        // Waiting until there is a task to execute or the pool is stopped
+                        cv.wait(lock, [this]{
+                            return !tasks.empty() || stopPool;
+                        });
+
+                        // Exit the thread if there are no tasks and pool is stopped
+                        if(tasks.empty() && stopPool){
+                            return;
+                        }
+
+                        // Get the next task from the queue
+                        task = move(tasks.front());
+                        tasks.pop();
+                    }
+
+                    task();
+                }
+            });
+        }
+    }
+
+    // Destructor to stop thread pool
+    ~ThreadPool()
+    {
+        {
+            // Lock the queue to update the flag safely
+            unique_lock<mutex> lock(queue_mutex);
+            stopPool = true;
+        }
+
+        // Notify all threads
+        cv.notify_all();
+
+        // Joining all worker threads to ensure that they have completed their tasks
+        for(auto& thread : threads){
+            thread.join();
+        }
+    }
+
+    // Enqueue task for execution by the thread pool
+    void enqueue(function<void()> task)
+    {
+        {
+            unique_lock<mutex> lock(queue_mutex);
+            tasks.emplace(move(task));
+        }
+        cv.notify_one();
+    }
+
+
+    private:
+
+    // Vector to store worker threads
+    vector<thread> threads;
+
+    // Queue of tasks
+    queue<function<void()>> tasks;
+
+    // Mutex to synchronize access to shared data
+    mutex queue_mutex;
+
+    // Condition variable to signal changes in the state of tasks queue
+    condition_variable cv;
+
+    // Flag to indicate whether thread pool should stop or not
+    bool stopPool = false;
+};
+
+// cout Mutex
+mutex cout_mutex;
+
+// Deadlock Example (Two Mutexes)
+mutex mt1, mt2;
+
+// Thread1 function
+void thread1() {
+    lock_guard<mutex> lock1(mt1);
+    this_thread::sleep_for(chrono::milliseconds(500));
+
+    lock_guard<mutex> lock2(mt2);
+    cout << "Thread1 finished!" << endl;
+}
+
+// Thread2 function
+void thread2() {
+    lock_guard<mutex> lock2(mt2);
+    this_thread::sleep_for(chrono::milliseconds(500));
+
+    lock_guard<mutex> lock1(mt1);
+    cout << "Thread2 finished!" << endl;
+}
+
+// Deadlock-free locking
+void safeTask(){
+    lock(mt1, mt2);
+
+    lock_guard<mutex> lock1(mt1, adopt_lock);
+    lock_guard<mutex> lock2(mt2, adopt_lock);
+
+    cout << "Tasks executed safely\n";
+}
+
+
+
 int main() {
 
     // // Create new thread
@@ -91,6 +226,7 @@ int main() {
     // // Main thread sleeping for 1 second
     // cout << "Main thread sleeping for 1 second..." << endl;
     // this_thread::sleep_for(chrono::seconds(1));
+
 
 
     // int n = 3;
@@ -122,7 +258,7 @@ int main() {
     // consumer_thread.join();
     // producer_thread.join();
 
-    
+
 
     // // Create 5 Threads → Observe Race Condition & Fix Using mutex
     // thread t1(getId, 1);
@@ -137,6 +273,51 @@ int main() {
     // t4.join();
     // t5.join();
 
-    cout << "Main thread awake and finished!";
+
+
+    // // Launch the function asynchronously and get a future
+    // future<int> future_value = async(launch::async, get_the_answer);
+
+    // // Do other work while waiting
+    // cout << "Doing other work..." << endl;
+
+    // // Wait for the result and get it
+    // // The call to .get() will block if the result isn't ready yet
+    // cout << "Future value: " << future_value.get() << endl;
+
+
+
+    // // Using future with promise
+    // promise<int> p;
+    // future<int> f1 = p.get_future();
+    // thread t(get_answer, move(p));
+    // t.join();
+    // cout << "Future value: " << f1.get() << endl;
+
+
+
+    // // Create a thread pool with 4 threads
+    // ThreadPool pool(4);
+
+    // // Enqueue tasks for execution
+    // for(int i = 0; i < 5; ++i){
+    //     pool.enqueue([i]{
+    //         unique_lock<mutex> lock(cout_mutex);
+    //         cout << "Task " << i << " is running by Thread " << this_thread::get_id() << endl;
+
+    //         // Simulate some work
+    //         this_thread::sleep_for(chrono::microseconds(10));
+    //     });
+    // }
+
+
+
+    // // Deadlock Example (Two Mutexes)
+    // thread t1(safeTask);
+    // thread t2(safeTask);
+    // t1.join();
+    // t2.join();
+
+    cout << "Main thread awake and finished!" << endl;
     return 0;
 }
